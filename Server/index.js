965d9f8c82ddec1a825cc5c9d1986efc945f1bc1
Aman Kumar Singh <singhkumaraman17@gmail.com>
2024-10-28 13:42:39 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -7,6 +7,12 @@ import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+console.log(__dirname)
+
 
 // to use .env file
 dotenv.config();
@@ -19,8 +25,6 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-const __dirname = path.resolve();
 
 const app = express();
 
@@ -36,11 +40,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, "client/dist")))
+
+app.get('*', (req,res)=> res.sendFile(__dirname, "client/dist/index.html"))
+
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
