32a2ffe022d9cbfb909cbb6a7196149e5d33ed11
Aman Kumar Singh <singhkumaraman17@gmail.com>
2024-10-28 13:12:15 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://my-blog-5a1v.onrender.com',
         secure: false,
       },
     },
